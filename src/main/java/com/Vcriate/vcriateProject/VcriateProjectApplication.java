800135154d1a8941acb51ccbc0d21071d2d3f760
sasank <saisasank2323@gmail.com>
2025-05-13 14:28:53 +0530

    first commit

---
+++ src/main/java/com/Vcriate/vcriateProject/VcriateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Vcriate.vcriateProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcriateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcriateProjectApplication.class, args);
+	}
+
+}
